84b48ce6b3f114578ba4934f0ec1cced36b1b8a8
Akriti <upadhayaakriti23@gmail.com>
2024-12-02 22:13:52 +0545

    made relations

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
-    //
+    protected $fillable=[
+        'name',
+        'details',
+        'admin_id',
+        'last_message_id',
+    ];
+    public function users(){
+        return $this->belongsToMany(User::class, 'group_users');
+    }
+    public function messages(){
+        return $this->hasMany(Message::class);
+    }
+    public function owner(){
+        return $this->hasMany(User::class);
+    }
 }
