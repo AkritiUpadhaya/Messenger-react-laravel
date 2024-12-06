f047d114828bee414ed994598902d43661e04f15
Akriti <upadhayaakriti23@gmail.com>
2024-12-06 22:02:43 +0545

    echo configuration

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,6 +1,7 @@
 import { usePage } from '@inertiajs/react'
-import React from 'react'
+import React, { useEffect } from 'react'
 import AuthenticatedLayout from './AuthenticatedLayout';
+import Echo from 'laravel-echo';
 
  function ChatLayout({children}) {
     const page= usePage();
@@ -8,6 +9,21 @@ import AuthenticatedLayout from './AuthenticatedLayout';
     const selectedConversation= page.props.selectedConversation;
     console.log('conversations', conversations);
     console.log('selectedConversation', selectedConversation);
+    useEffect(()=>{
+        Echo.join('online') 
+        .here((users)=>{
+          console.log('here', users);
+        })
+        .joining((user)=>{
+          console.log('joining', user);
+        })
+        .leaving((user)=>{
+          console.log('leaving', user);
+        }).error((error)=>{
+          console.error('error',error);
+        });
+        
+    },[]);
   return (
     <>
    
