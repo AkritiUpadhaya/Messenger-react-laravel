b7f5bcceeb8a735ca589dc143b2dd148b7e67605
Akriti <upadhayaakriti23@gmail.com>
2024-12-11 22:35:42 +0545

    wip-displaying conversations

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -8,8 +8,43 @@ import Echo from 'laravel-echo';
     const conversations= page.props.conversations;
     const selectedConversation= page.props.selectedConversation;
     const [online, setOnline]= useState({});
+    const [localConversation, setLocalConversation]= useState(conversations);
+    const [sortedConversation, setSortedConversation]= useState([])
     console.log('conversations', conversations);
     console.log('selectedConversation', selectedConversation);
+    const isUserOnline= (userId)=>online[userId];
+    useEffect(()=>{
+      setSortedConversation(
+        localConversation.sort((a,b)=>{
+          if(a.blocked_at && b.blocked_at){
+            return a.blocked_at > b.blocked_at? 1 : -1;
+          }
+          else if(a.blocked_at){
+            return 1;
+          }
+          else if (b.blocked_at){
+            return -1;
+          }
+          if(a.last_message_date && b.last_message_date){
+            return b.last_message_date.localComapare(
+              a.last_message_date);
+          }
+          else if(a.last_message_date){
+            return -1;
+          }
+          else if(b.last_message_date){
+            return 1;
+          }
+          else{
+            return 0;
+          }
+
+        })
+      )
+    }, [localConversation])
+    useEffect(()=>{
+      setLocalConversation(conversations)
+    }, [conversations])
     useEffect(()=>{
         window.Echo.join(`online`)
         .here((users)=>{
