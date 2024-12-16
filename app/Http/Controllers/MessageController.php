4d2321a28a1111fa62451b806052a9622f125353
Akriti <upadhayaakriti23@gmail.com>
2024-12-16 20:15:31 +0545

    updated Message controller and resources

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,21 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreMessageRequest;
+use App\Http\Resources\MessageResource;
 use App\Models\Message;
+use App\Models\Group;
+use App\Models\User;
+use Inertia\Inertia;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
 
 
 class MessageController extends Controller
 {
-    public function byUser(User $user){
-        $messages= Message::where('sender_id',auth()->id())
+    public function byUser(User $user)
+    {
+        $messages= Message::where('sender_id', Auth::id())
         ->where('receiver_id', $user->id)
         ->orWhere('sender_id', $user->id)
-        ->where('receiver_id',auth()->id())
+        ->where('receiver_id',Auth::id())
         ->latest()
         ->paginate(10);
         return inertia('Home',[
             'selectedConversation'=>$user->toconversationArray(),
+            'messages'=> MessageResource::collection($messages)
         ]);
     } 
     public function byGroup(Group $group){
