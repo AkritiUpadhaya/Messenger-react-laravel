947b4ab45562432bd2ed49aee38e4eba2fcb9f8e
Akriti <upadhayaakriti23@gmail.com>
2024-12-17 22:51:24 +0545

    updated app components

--- resources/js/Pages/HomePage.jsx
+++ resources/js/Pages/HomePage.jsx
@@ -1,12 +1,52 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import ChatLayout from '@/Layouts/ChatLayout';
+import { ChatBubbleLeftRightIcon } from '@heroicons/react/16/solid';
 
 import { Head } from '@inertiajs/react';
+import { useEffect, useRef, useState } from 'react';
 
-function HomePage() {
+function HomePage({messages}) {
+    const [localMessages, setLocalMessages]= useState([]);
+    const messagesCtrRef= useRef(null);
+    useEffect(()=>{
+        setLocalMessages(messages)
+    }, [messages])
     return (
         <>
-          Messages</>
+        {!messages && (
+            <div className='flex flex-col gap-8 justify-center items-center text-center h-full opacity-35'>
+                <div className='text-2xl md:text-4xl p-16 text-slate-900'>
+                    Please select the conversation to select the messages
+                </div>
+                <ChatBubbleLeftRightIcon className='w-32 h-32 inline-block' />
+            </div>
+             )}
+            {messages && (
+                <>
+                <ConversationHeader 
+                selectedConversation= {selectedConversation}/>
+                <div ref={messagesCtrRef} className='flex-1 overflow-y-auto p-5'>
+                    {localMessages.length==0 && (
+                        <div className='flex justify-center items-center h-full'>
+                            <div className='text-lg text-slate-200'>
+                                No messages found
+                            </div>
+                        </div>
+                    )}
+                    {localMessages.length>0 && (
+                        <div className='flex-1 flex flex-col'>
+                            {localMessages.map((message)=>(
+                                <MessageItem key={message.id}
+                                message= {message} />
+                            ))}
+                        </div>
+                    )}
+                </div>
+                <MessageInput conversation={selectedConversation}/>
+                </>
+            )}
+       
+        </>
          
     );
 }
